a03f1e7a12a7c1a297aeba1ac8167a3f3febf7cd
ChristoferMendes <christoferluizdsm@gmail.com>
2025-01-24 15:02:05 -0300

    feat: add tooltip to recording type

--- src/components/RecordingTypesList.tsx
+++ src/components/RecordingTypesList.tsx
@@ -2,14 +2,19 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import React from "react";
 import { useGetRecordingTypes } from "@/hooks/useGetRecordingTypes";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { CircleHelp } from "lucide-react";
 
 export function RecordingTypesList() {
   const { recordingTypes, setSelectedRecording, selectedRecording } = useGetRecordingTypes();
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="relative flex items-center space-x-2">
       <Label>Recording Type:</Label>
-      <Select onValueChange={setSelectedRecording} defaultValue={selectedRecording}>        <SelectTrigger className="w-[180px]">
+
+      <Select onValueChange={setSelectedRecording} defaultValue={selectedRecording}>
+        {" "}
+        <SelectTrigger className="w-[180px]">
           <SelectValue placeholder="Select type" />
         </SelectTrigger>
         <SelectContent>
@@ -23,6 +28,17 @@ export function RecordingTypesList() {
           ))}
         </SelectContent>
       </Select>
+      <TooltipProvider delayDuration={300}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <CircleHelp size={22} />
+          </TooltipTrigger>
+          <TooltipContent className={'w-72'}>
+            This type is used to make automatic folders in your ~/Videos folder. You can create and customize type in the
+            Recording Types tab
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 }
