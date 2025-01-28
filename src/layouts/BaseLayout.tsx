3e76e751260b2590785a5bf1a9bf023c75b39745
ChristoferMendes <christoferluizdsm@gmail.com>
2025-01-28 17:43:11 -0300

    refactor: rename project from electron-shadcn to Kronos

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import DragWindowRegion from "@/components/DragWindowRegion";
-import NavigationMenu from "@/components/NavigationMenu";
 import { Toaster } from "sonner";
 
 export default function BaseLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <DragWindowRegion title="electron-shadcn" />
+      <DragWindowRegion title="Kronos" />
       <Toaster />
-      {/*<NavigationMenu />*/}
       <hr />
       <main>{children}</main>
     </>
